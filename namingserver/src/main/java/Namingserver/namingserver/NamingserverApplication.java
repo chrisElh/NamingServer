9042cae62ba7684bf038e7ca80d47d884b15744a
chrisElh <christian-eh@hotmail.com>
2025-05-09 14:37:16 +0200

    initial commit

---
+++ namingserver/src/main/java/Namingserver/namingserver/NamingserverApplication.java
@@ -0,0 +1,13 @@
+package Namingserver.namingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamingserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamingserverApplication.class, args);
+	}
+
+}
