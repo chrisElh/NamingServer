1ab3a83e60385adc4b0a1122b8eb8849f27cbec4
Seti_RF <setara.raufi@student.uantwerpen.be>
2025-05-17 20:43:16 +0200

    working on file transfer via TCP

--- namingserver/src/main/java/NodePackage/NodeClients/TestNode1.java
+++ namingserver/src/main/java/NodePackage/NodeClients/TestNode1.java
@@ -9,15 +9,16 @@ import java.net.URL;
 public class TestNode1 {
     public static void main(String[] args) {
         NodeApp app = new NodeApp();
-        Node node = app.createAndAnnounceNewNode("TestNode1", 3030, "C:\\Users\\chris\\IdeaProjects\\NServer\\namingserver\\src\\main\\resources" );
+        URL resource = TestNode1.class.getClassLoader().getResource("files1");
+        if (resource == null) {
+            System.err.println("Directory not found in resources");
+            return;
+        }
+        File dir = new File(resource.getFile());
+        Node node = app.createAndAnnounceNewNode("TestNode1", 3030, dir.getAbsolutePath() );
         System.out.println(node.getLocalFileNames());
-//        URL resource = TestNode1.class.getClassLoader().getResource("files");
-//        if (resource == null) {
-//            System.err.println("Directory not found in resources");
-//            return;
-//        }
-//        File dir = new File(resource.getFile());
-//
+
+
 //        node.loadLocalFilesFromDirectory(dir.getAbsolutePath());
 
 
--- namingserver/src/main/java/NodePackage/NodeClients/TestNode2.java
+++ namingserver/src/main/java/NodePackage/NodeClients/TestNode2.java
@@ -1,10 +1,22 @@
 package NodePackage.NodeClients;
 
+import NodePackage.Node;
 import NodePackage.NodeApp;
+
+import java.io.File;
+import java.net.URL;
 
 public class TestNode2 {
     public static void main(String[] args) {
         NodeApp app = new NodeApp();
       //  app.createAndAnnounceNewNode("TestNode2", 2050);
+        URL resource = TestNode1.class.getClassLoader().getResource("files2");
+        if (resource == null) {
+            System.err.println("Directory not found in resources");
+            return;
+        }
+        File dir = new File(resource.getFile());
+        Node node = app.createAndAnnounceNewNode("TestNode1", 2050, dir.getAbsolutePath() );
+        System.out.println(node.getLocalFileNames());
     }
 }
