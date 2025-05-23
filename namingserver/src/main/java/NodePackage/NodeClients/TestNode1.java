a48f7aeed0f573f40a2606d77a79a1925b6cf6e5
Seti_RF <setara.raufi@student.uantwerpen.be>
2025-05-23 20:02:41 +0200

    main(local)

--- namingserver/src/main/java/NodePackage/NodeClients/TestNode1.java
+++ namingserver/src/main/java/NodePackage/NodeClients/TestNode1.java
@@ -10,7 +10,8 @@ public class TestNode1 {
     public static void main(String[] args) {
         NodeApp app = new NodeApp();
        // Node node = app.createAndAnnounceNewNode("Christian", 3030, "C:\\Users\\chris\\IdeaProjects\\NServer\\namingserver\\src\\main\\resources\\files1\\local", "C:\\Users\\chris\\IdeaProjects\\NServer\\namingserver\\src\\main\\resources\\files1\\replicas");
-        Node node = app.createAndAnnounceNewNode("Christian", 3030, "C:\\Users\\edwar\\Universiteit\\6-DIST\\Practicum\\Lab5\\Replication\\namingserver\\src\\main\\resources\\files1\\local", "C:\\Users\\edwar\\Universiteit\\6-DIST\\Practicum\\Lab5\\Replication\\namingserver\\src\\main\\resources\\files1\\replica");
+    //    Node node = app.createAndAnnounceNewNode("Christian", 3030, "C:\\Users\\edwar\\Universiteit\\6-DIST\\Practicum\\Lab5\\Replication\\namingserver\\src\\main\\resources\\files1\\local", "C:\\Users\\edwar\\Universiteit\\6-DIST\\Practicum\\Lab5\\Replication\\namingserver\\src\\main\\resources\\files1\\replica");
+        Node node = app.createAndAnnounceNewNode("Christian", 3030, "C:\\3de_jaar\\3_Distributed_Systeem\\Lab5\\namingserver\\src\\main\\resources\\files1\\local", "C:\\3de_jaar\\3_Distributed_Systeem\\Lab5\\namingserver\\src\\main\\resources\\files1\\replica");
 
         System.out.println(node.getLocalFileNames());
 
--- namingserver/src/main/java/NodePackage/NodeClients/TestNode3.java
+++ namingserver/src/main/java/NodePackage/NodeClients/TestNode3.java
@@ -19,7 +19,8 @@ public class TestNode3 {
 //        }
 //        File dir = new File(resource.getFile());
      //   Node node = app.createAndAnnounceNewNode("Edward", 2570, "C:\\Users\\chris\\IdeaProjects\\NServer\\namingserver\\src\\main\\resources\\files3\\local", "C:\\Users\\chris\\IdeaProjects\\NServer\\namingserver\\src\\main\\resources\\files3\\replicas");
-        Node node = app.createAndAnnounceNewNode("Tadiwos", 4060, "C:\\Users\\edwar\\Universiteit\\6-DIST\\Practicum\\Lab5\\Replication\\namingserver\\src\\main\\resources\\files3\\local", "C:\\Users\\edwar\\Universiteit\\6-DIST\\Practicum\\Lab5\\Replication\\namingserver\\src\\main\\resources\\files3\\replica");
+        //Node node = app.createAndAnnounceNewNode("Tadiwos", 4060, "C:\\Users\\edwar\\Universiteit\\6-DIST\\Practicum\\Lab5\\Replication\\namingserver\\src\\main\\resources\\files3\\local", "C:\\Users\\edwar\\Universiteit\\6-DIST\\Practicum\\Lab5\\Replication\\namingserver\\src\\main\\resources\\files3\\replica");
+        Node node = app.createAndAnnounceNewNode("Edward", 4060, "C:\\3de_jaar\\3_Distributed_Systeem\\Lab5\\namingserver\\src\\main\\resources\\files3\\locale", "C:\\3de_jaar\\3_Distributed_Systeem\\Lab5\\namingserver\\src\\main\\resources\\files3\\replica");
 
         System.out.println(node.getLocalFileNames());
 //        Node node = app.createAndAnnounceNewNode("TestNode2", 2000);
